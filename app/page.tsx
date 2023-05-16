e97c4cd3980e9a9670e806e6f58a77060ddcfbd7
kalenluciano <103911842+kalenluciano@users.noreply.github.com>
2023-05-16 16:00:39 -0400

    Set up structure for filter form component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import FilterForm from '@/components/FilterForm'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <main className={styles.main}></main>
+  return <main className={styles.main}>
+    <FilterForm/>
+  </main>
 }
