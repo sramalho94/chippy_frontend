bd148b521b0c8890a6d4b646a67053e0663109b3
Shondriane <shondriane.wise@yahoo.com>
2023-05-17 15:58:45 -0400

    creating profile component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import Profile from '@/components/Profile'
 
 const inter = Inter({ subsets: ['latin'] })
-
+const user={}
 export default function Home() {
-  return <main className={styles.main}></main>
+  return <main className={styles.main}>
+    <Profile user={{user}}/>
+  </main>
 }
